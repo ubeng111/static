df8315720ea2b336111e855eaa47e20b43327062
Ubeng111 <akangvillapuncak@gmail.com>
2025-06-01 13:58:27 +0700

    Fix bug pada halaman login

--- app/(others)/404/page.jsx
+++ app/(others)/404/page.jsx
@@ -1,35 +1,23 @@
-import dynamic from "next/dynamic";
-import CallToActions from "@/components/common/CallToActions";
 import DefaultHeader from "@/components/header/default-header";
 import DefaultFooter from "@/components/footer/default";
-import NotFound from "@/components/common/NotFound";
+import CallToActions from "@/components/common/CallToActions";
+import NotFound from "@/components/common/NotFound"; // Import the NotFound component
 
 export const metadata = {
   title: "404 || GoTrip - Travel & Tour React NextJS Template",
-  description: "GoTrip - Travel & Tour React NextJS Template",
+  description: "Sorry, the page you're looking for doesn't exist.",
 };
 
-const index = () => {
+const Page = () => {
   return (
     <>
-      {/* End Page Title */}
-
       <div className="header-margin"></div>
-      {/* header top margin */}
-
       <DefaultHeader />
-      {/* End Header 1 */}
-
-      <NotFound />
-      {/* End 404 section */}
-
+      <NotFound />  {/* Display the NotFound component */}
       <CallToActions />
-      {/* End Call To Actions Section */}
-
       <DefaultFooter />
-      {/* End Call To Actions Section */}
     </>
   );
 };
 
-export default dynamic(() => Promise.resolve(index), { ssr: false });
+export default Page;
