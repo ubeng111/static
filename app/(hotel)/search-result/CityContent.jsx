479f4b7f5b2c33f4bde4f8ad64cc759b88eab26f
Ubeng111 <akangvillapuncak@gmail.com>
2025-06-03 22:45:20 +0700

    Figg gin

--- app/(hotel)/search-result/CityContent.jsx
+++ app/(hotel)/search-result/CityContent.jsx
@@ -98,31 +98,6 @@ export default function CityContent({ initialCityId, initialPage }) {
 
   return (
     <>
-      <section className="section-bg pt-40 pb-40 relative z-5">
-        <div className="section-bg__item col-12">
-          <img
-            src="/img/misc/bg-1.webp"
-            srcSet="/img/misc/bg-1.webp 480w, /img/misc/bg-1.webp 768w, /img/misc/bg-1.webp 1200w"
-            alt="Gambar latar mewah"
-            loading="lazy"
-            className="w-full h-full object-cover"
-            width="1200"
-            height="800"
-          />
-        </div>
-        <div className="container">
-          <div className="row">
-            <div className="col-12">
-              <div className="text-center">
-                <h1 className="text-25 fw-600 text-white">
-                  Search Result Properties In {cityName}
-                </h1>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-
       <section className="layout-pt-md layout-pb-lg">
         <div className="container">
           <div className="row">
