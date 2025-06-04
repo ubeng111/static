f8cf4e42141fe3e3d833bfe89deb5e4ca7321095
Ubeng111 <akangvillapuncak@gmail.com>
2025-06-04 14:01:11 +0700

    Isaaat

--- components/hotel-single/GalleryTwo.jsx
+++ components/hotel-single/GalleryTwo.jsx
@@ -19,8 +19,7 @@ const GalleryTwo = ({ hotel }) => {
           <div className="col-auto">
             <div className="row x-gap-10 sm:x-gap-20 items-center">
               <div className="col-auto">
-                <h1 className="text-24 sm:text-28 md:text-30 fw-600">{hotel?.title?.slice(0, 30)}</h1>
-              </div>
+<h1 className="text-24 sm:text-28 md:text-30 fw-600">{hotel?.title}</h1>              </div>
               <div className="col-auto">
                 {[...Array(5)].map((_, i) => (
                   <i key={i} className="icon-star text-10 text-yellow-1" />
@@ -69,8 +68,8 @@ const GalleryTwo = ({ hotel }) => {
         {hotel?.overview && (
           <div className="pt-20 sm:pt-30 md:pt-40 px-0">
             <h2 className="text-20 sm:text-22 fw-500 border-top-light mb-10 sm:mb-20">
-              Overview {hotel?.title?.slice(0, 30)}
-            </h2>
+  Overview {hotel?.title}
+</h2>
             {(() => {
               const sentences = hotel.overview.split(".").filter(Boolean);
               const partLength = Math.ceil(sentences.length / 3);
