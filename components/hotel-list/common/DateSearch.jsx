cd614fba5ec5313ab3430d48eaf139b28184a1a8
Ubeng111 <akangvillapuncak@gmail.com>
2025-06-08 14:33:23 +0700

    kkt

--- components/hotel-list/common/DateSearch.jsx
+++ components/hotel-list/common/DateSearch.jsx
@@ -38,8 +38,7 @@ const DateSearch = ({ onDateChange }) => {
   return (
     <div className="searchMenu-date search-field">
       {/* Label yang terhubung secara programatis dan disembunyikan secara visual */}
-      {/* Tetapkan ID unik untuk label dan gunakan sr-only untuk menyembunyikan secara visual */}
-      <label id={labelId} htmlFor={`${labelId}-input`} className="sr-only">Check-in - Check-out</label>
+      <label id={labelId} className="sr-only">Check-in - Check-out</label>
       <DatePicker
         value={dates}
         onChange={handleDateChange}
@@ -50,11 +49,10 @@ const DateSearch = ({ onDateChange }) => {
         inputClass="w-full h-32 px-6 py-2"
         containerStyle={{ width: '100%' }}
         calendarPosition="bottom-left"
-        // Gunakan 'aria-labelledby' untuk mengaitkan input dengan label tersembunyi
         inputProps={{
-          id: `${labelId}-input`, // Berikan ID yang unik untuk input yang dirender
-          placeholder: "Check-in ~ Jun 09", // Sesuaikan placeholder jika perlu
-          'aria-labelledby': labelId // Ini akan mengaitkan input dengan label tersembunyi
+          id: `${labelId}-input`,
+          placeholder: "Check-in ~ Check-out",
+          'aria-label': 'Check-in and Check-out dates', // Fallback jika aria-labelledby tidak bekerja
         }}
       />
     </div>
