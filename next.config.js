9f1aed72f46a119ea2cf793081fc662107dd11f2
Ubeng111 <akangvillapuncak@gmail.com>
2025-06-17 11:12:26 +0700

    Configure Yarn Berry and synchronize lockfile for Cloud Worker compatibility

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+    output: 'export',
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'pix1.agoda.net' },
