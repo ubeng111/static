d93310a735ff87d987c0703938f565e783554e2c
Ubeng111 <akangvillapuncak@gmail.com>
2025-06-18 18:25:18 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -1,31 +1,31 @@
 const path = require('path');
 
-// Hapus atau komentari baris ini setelah analisis selesai
-// const withBundleAnalyzer = require('@next/bundle-analyzer')({
-//   enabled: process.env.ANALYZE === 'true',
-// });
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
+      { protocol: 'http', hostname: 'pix1.agoda.net' },
       { protocol: 'https', hostname: 'pix1.agoda.net' },
-      { protocol: 'https', hostname: 'pix2.agoda.net' },
-      { protocol: 'https', hostname: 'pix3.agoda.net' },
-      { protocol: 'https', hostname: 'pix4.agoda.net' },
-      { protocol: 'https', hostname: 'pix5.agoda.net' },
-      { protocol: 'https', hostname: 'pix8.agoda.net' },
+      { protocol: 'http', hostname: 'q-xx.bstatic.com' },
       { protocol: 'https', hostname: 'q-xx.bstatic.com' },
+      { protocol: 'http', hostname: 'pix2.agoda.net' },
+      { protocol: 'https', hostname: 'pix2.agoda.net' },
+      { protocol: 'http', hostname: 'pix3.agoda.net' },
+      { protocol: 'https', hostname: 'pix3.agoda.net' },
+      { protocol: 'http', hostname: 'pix4.agoda.net' },
+      { protocol: 'https', hostname: 'pix4.agoda.net' },
+      { protocol: 'http', hostname: 'pix5.agoda.net' },
+      { protocol: 'https', hostname: 'pix5.agoda.net' },
+      { protocol: 'http', hostname: 'pix8.agoda.net' },
       { protocol: 'https', hostname: 'flagcdn.com' },
+      { protocol: 'https', hostname: 'pix8.agoda.net' },
     ],
   },
   webpack: (config, { isServer }) => {
     config.resolve.alias['@components'] = path.join(__dirname, 'components');
     return config;
   },
-  output: 'standalone', // Tetap pertahankan ini
 };
 
-// Ubah kembali cara export
-module.exports = nextConfig; // Kembali ke nextConfig secara langsung
+module.exports = nextConfig;
